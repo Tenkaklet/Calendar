1b9e95e17e021b8bf98e54296687ec31b2eb1dcb
Tenkaklet <tenkaklet@gmail.com>
2016-01-01 19:49:25 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,51 @@
+var calendarApp = angular.module('calendarDemoApp', []);
+calendarApp.controller('MyCtrl', function ($scope){
+
+    $scope.months = {
+        0: 'January',
+        1: 'Febuary',
+        2: 'March',
+        3: 'April',
+        4: 'May',
+        5: 'June',
+        6: 'July',
+        7: 'August',
+        8: 'September',
+        9: 'October',
+        10: 'November',
+        11: 'December'
+    };
+    var today = new Date();
+    var year = today.getFullYear();
+    var month = today.getMonth();
+
+
+    var range = CalendarRange.getMonthlyRange(new Date());
+    // console.log(range);
+    $scope.firstDays = angular.forEach(range.days, function(days) {
+        // console.log(days);
+        $scope.dates = {
+            year: days.year,
+            day: days.day
+        };
+        var years = [];
+        var then = year - 100;
+        for(var i=then;i<=year;i++) {
+            years.push(i);
+        }
+        $scope.years = years;
+        // console.log($scope.years);
+        console.log($scope.dates);
+    });
+});
+
+
+// console.log(firstDays);
+// console.log(range);
+// your controller and directive code go here
+calendarApp.directive('theCalendar', function() {
+    return {
+        restrict: 'E',
+        templateUrl: 'calendar.html'
+    };
+});
